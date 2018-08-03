24687eb183a98312757b8e74b8703bf158b553d7
Близнюк Дмитрий <dmitrij.bliznjuk@privatbank.ua>
2018-08-03 17:53:05 +0300

    HW5 Preview HW

--- src/com/blyzniukd/hw/storage/AbstractArrayStorage.java
+++ src/com/blyzniukd/hw/storage/AbstractArrayStorage.java
@@ -1,46 +1,55 @@
 package com.blyzniukd.hw.storage;
 
-import com.blyzniukd.hw.exception.ExistStorageException;
 import com.blyzniukd.hw.exception.NotExistStorageException;
 import com.blyzniukd.hw.exception.StorageException;
 import com.blyzniukd.hw.model.Resume;
 
 import java.util.Arrays;
 
-public abstract class AbstractArrayStorage implements Storage {
-    protected static final int STORAGE_LIMIT = 10;
+public abstract class AbstractArrayStorage extends AbstractStorage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
-    protected int size = 0;
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index > -1) {
-            storage[index] = resume;
-        } else {
-            throw new NotExistStorageException(resume.getUuid());
-        }
+    @Override
+    protected void doUpdate(Object index, Resume resume) {
+        storage[(Integer) index] = resume;
     }
 
-    public void save(Resume resume) {
-        int index = getIndex(resume.getUuid());
+    @Override
+    protected void doSave(Object index, Resume resume) {
         if (size < STORAGE_LIMIT) {
-            if (index < 0) {
-                insertResume(resume, index);
+            if ((Integer) index < 0) {
+                insertResume(resume, (Integer)index);
                 size++;
-            } else {
-                throw new ExistStorageException(resume.getUuid());
             }
         } else {
             throw new StorageException("The array is full. Try to delete element from array before save.", resume.getUuid());
         }
+
     }
 
-    public Resume get(String uuid) {
+    @Override
+    protected void doDelete(Object index) {
+            deleteResume((Integer)index);
+            storage[size - 1] = null;
+            size--;
+    }
+
+    @Override
+    protected boolean isExistElement(Object index) {
+        return (Integer) index>=0;
+    }
+
+    @Override
+    protected Resume getElement(Object index) {
+        return storage[(Integer) index];
+    }
+
+    public Resume doGet(String uuid) {
         int index = getIndex(uuid);
         if (index > -1) {
             return storage[index];
@@ -64,13 +73,11 @@ public abstract class AbstractArrayStorage implements Storage {
         return Arrays.copyOf(storage, size);
     }
 
-    public int size() {
-        return size;
-    }
-
     protected abstract int getIndex(String uuid);
 
     protected abstract void insertResume(Resume resume, int index);
 
     protected abstract void deleteResume(int index);
+
+
 }
