30f762502191bb5f5cc5d7f30d3e6e4ff0745aeb
Близнюк Дмитрий <dmitrij.bliznjuk@privatbank.ua>
2018-08-23 16:33:32 +0300

    HW6 Implementation of MapStorage (without getAll()).

--- src/com/blyzniukd/hw/storage/MapStorage.java
+++ src/com/blyzniukd/hw/storage/MapStorage.java
@@ -3,42 +3,47 @@ package com.blyzniukd.hw.storage;
 import com.blyzniukd.hw.model.Resume;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
+
+    private Map<String, Resume> storage = new HashMap<String, Resume>();
+
     @Override
     protected Object findKey(String uuid) {
-        return null;
+        return storage.get(uuid);
     }
 
     @Override
     protected Resume doGet(Object keyObject) {
-        return null;
+        return storage.get(keyObject);
     }
 
     @Override
     protected boolean isExist(Object keyObject) {
-        return false;
+        return storage.containsKey(keyObject);
     }
 
     @Override
     protected void doUpdate(Object keyObject, Resume resume) {
-
+        this.doSave(keyObject, resume);
     }
 
     @Override
     protected void doSave(Object keyObject, Resume resume) {
-
+        storage.put(((Resume) keyObject).getUuid(), resume);
     }
 
     @Override
     protected void doDelete(Object keyObject) {
-
+        storage.remove(keyObject);
     }
 
     @Override
     public void clear() {
-
+        storage.clear();
     }
 
     @Override
@@ -48,6 +53,6 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     public int size() {
-        return 0;
+        return storage.size();
     }
 }
